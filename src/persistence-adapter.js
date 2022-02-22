e98f65f4a857074d4744859d27c27db236e5eab2
Paolo <diciaula.paolo@gmail.com>
2022-02-22 13:31:55 +0100

    clean useless files

--- src/persistence-adapter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PersistenceAdapter = void 0;
-class PersistenceAdapter {
-}
-exports.PersistenceAdapter = PersistenceAdapter;
